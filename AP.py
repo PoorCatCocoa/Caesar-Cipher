1b12bc95596df0713a05f838ef6b12f0fcc85c57
PoorCatCocoa <1697685305w@gmail.com>
2022-04-30 15:26:07 -0400

    整合

--- AP.py
+++ AP.py
@@ -1,38 +1,55 @@
-ciphertext = ''
-key = ''
-decrypted = ''
+choice = ''
+while choice != 'Q' or 'q':
+    choice = input("Enter 'E' to encripte; 'D' to decripte; 'Esc' to exit the programme")
+    if choice == 'E' or 'e':
+        o_mes = 'abc'
+        en_key = 5
+        en_mes = ''
 
-for c in ciphertext:
-
-    if c.isupper():
-
-        c_index = ord(c) - ord('A')
-
-        # shift the current character to left by key positions to get its original position
-        c_og_pos = (c_index - key) % 26 + ord('A')
-
-        c_og = chr(c_og_pos)
-
-        decrypted += c_og
-
-    elif c.islower():
-
-        c_index = ord(c) - ord('a')
-
-        c_og_pos = (c_index - key) % 26 + ord('a')
-
-        c_og = chr(c_og_pos)
-
-        decrypted += c_og
-
-    elif c.isdigit():
-
-        # if it's a number,shift its actual value
-        c_og = (int(c) - key) % 10
-
-        decrypted += str(c_og)
-
+        for en_char in o_mes:
+            if en_char.isupper():
+                en_index = ord(en_char) - ord('A')
+                en_shift = (en_index + en_key) % 26 + ord('A')
+                en_range = chr(en_shift)
+                en_mes += en_range
+            elif en_char.islower():
+                en_index = ord(en_char) - ord('a')
+                en_shift = (en_index + en_key) % 26 + ord('a')
+                en_range = chr(en_shift)
+                en_mes += en_range
+            elif en_char.isdigit():
+                en_range = (int(en_char) + en_key) % 10
+                en_mes += str(en_range)
+            else:
+                en_mes += en_char
+        print("\n", o_mes + '\n', en_key, '\n', en_mes)
+    elif choice == "D" or 'd':
+        en_mes = 'fgh'
+        de_key = 25
+        num = 0
+        de_mes = ''
+        while de_key != -1:
+            de_key = de_key - 1
+            num = num + 1
+            for de_char in en_mes:
+                if de_char.isupper():
+                    de_index = ord(de_char) - ord('A')
+                    de_og_pos = (de_index - de_key) % 26 + ord('A')
+                    de_og = chr(de_og_pos)
+                    de_mes += de_og
+                elif de_char.islower():
+                    de_index = ord(de_char) - ord('a')
+                    de_og_pos = (de_index - de_key) % 26 + ord('a')
+                    de_og = chr(de_og_pos)
+                    de_mes += de_og
+                elif de_char.isdigit():
+                    de_og = (int(de_char) - de_key) % 10
+                    de_mes += str(de_og)
+                else:
+                    de_mes += de_char
+            print('#', num, de_mes + "\n")
+            de_mes = ''
+    elif choice != 'Q' or 'q':
+        print('faaf')
     else:
-
-        # if its neither alphabetical nor a number, just leave it like that
-        decrypted += c
+        break
